063f58fb59181aced3937449f57ef9a518df3ed8
Jiri Kuncar <jiri.kuncar@cern.ch>
2013-09-27 15:01:56 +0200

    global: new code structure
    * Adds Sphinx documentation.
    
    * Moves Jinja2 templates to the new code structure
      `invenio/modules/<name>/templates/<name>/index.html`.
    
    * Adds new tag module developed by Krzysztof Lis
      <krzysztof.lis@cern.ch>.
    
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Co-authored-by: Esteban J. G. Gabancho <esteban.gabancho@cern.ch>
    Co-authored-by: Krzysztof Lis <krzysztof.lis@cern.ch>
    Co-authored-by: Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
    
    NOTE: beware, patch amended by Tibor to use new file names

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2009, 2010, 2011, 2013 CERN.
+## Copyright (C) 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,27 +16,3 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
-"""Unit tests for BibKnowledge."""
-
-from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
-
-
-class BibknowledgeTests(InvenioTestCase):
-    """Unit test functions for bibknowledge."""
-    pass
-
-    def setUp(self):
-        """bibknowledge unit test setup"""
-        pass
-
-    def tearDown(self):
-        """bbibknowledge unit test cleanup"""
-        pass
-
-
-TEST_SUITE = make_test_suite(BibknowledgeTests)
-
-
-if __name__ == "__main__":
-    run_test_suite(TEST_SUITE)
